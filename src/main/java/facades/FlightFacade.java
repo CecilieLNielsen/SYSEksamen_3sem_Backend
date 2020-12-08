a468f0af10ccd779549fd7e859d25b3967baa327
Cecilie Nielsen <cph-cn274@cphbusiness.dk>
2020-12-08 14:48:32 +0100

    Get all flights by filter to frontend

--- src/main/java/facades/FlightFacade.java
+++ src/main/java/facades/FlightFacade.java
@@ -5,13 +5,17 @@
  */
 package facades;
 
+import DTO.FilterDTO;
 import DTO.FlightDTO;
 import entities.Airport;
 import entities.Flight;
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TemporalType;
 import javax.persistence.TypedQuery;
 
 /**
@@ -79,8 +83,35 @@ public class FlightFacade {
     public Flight getFlightEntityById(int id) {
         EntityManager em = emf.createEntityManager();
         try {
-            TypedQuery<Flight> query = em.createQuery("SELECT f FROM Flight f WHERE :id = :id", Flight.class).setParameter("id", id);
+            TypedQuery<Flight> query = em.createQuery("SELECT f FROM Flight f WHERE f.flightId = :id", Flight.class).setParameter("id", id);
             return query.getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<FlightDTO> getAllFlightsByFilter(FilterDTO filterDTO) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            // Jeg har prøvet at bruge TemporalType.DATE og prøvet at typecaste f.arrival til en date (sådan her: DATE(f.arrival)) Men jeg kunne ikke få dem til at virke (jeg har kommenteret dem ud)
+            TypedQuery<Flight> query = em.createQuery("SELECT f FROM Flight f WHERE f.takeoffAirport.country.countryName = :from AND f.destinationAirport.country.countryName = :to", Flight.class);// AND f.arrival = :arrival", Flight.class); // AND DATE(f.arrival) = :arrival", Flight.class);
+            query.setParameter("from", filterDTO.getFrom());
+            query.setParameter("to", filterDTO.getTo());
+            //query.setParameter("arrival", filterDTO.getArrival()); , TemporalType.DATE);
+
+            List<FlightDTO> flights = new ArrayList();
+            for (Flight f : query.getResultList()) {
+                // Grundet til skjulte timestamps i datoer kunne jeg ikke bruge equals metoden...
+                // Boolean compareDates = f.getArrival().equals(filterDTO.getArrival());
+                // Men jeg har kunne hente årstal, måned og dag ud og sammenligne dem sådan...
+                boolean compareDates = (f.getArrival().getYear() == filterDTO.getArrival().getYear()) 
+                        && (f.getArrival().getMonth() == filterDTO.getArrival().getMonth()) 
+                        && (f.getArrival().getDay() == filterDTO.getArrival().getDay());
+                if (compareDates) {
+                    flights.add(new FlightDTO(f));
+                }
+            }
+            return flights;
         } finally {
             em.close();
         }
